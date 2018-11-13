ed5303e39aa987344f62bcc6ec8e1ce82097c97a
Richard Allen White III <raw937@gmail.com>
2018-11-13 02:50:41 +0200

    Add files via upload

---
+++ bin/fig2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Written RAWIII
+
+##Align with mafft
+~/mafft-7.394-without-extensions/core/./mafft --localpair --maxiterate 1000 fig2_exi_16S_sequences.fasta >fig2_exi_16S_alignment.clust
+
+##Build phylogeny with iqtree
+~/iqtree-1.6.2-Linux/bin/iqtree -s fig2_exi_16S_alignment.clust -st DNA -m TEST -bb 1000 -alrt 1000
